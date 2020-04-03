7368088a1194f9c83ec1c3664c1faec2ae66004f
Bruno Viveiros <bruferviveiros@gmail.com>
2020-04-03 20:56:41 -0300

    configurado base do gatsby para o desenvolvimento

---
+++ config/metadata.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: `ROMSS`,
+  siteUrl: `https://github.com/BrunoViveiros/rom-ss`,
+  description: `A Ragnarok Online Eternal Love Mobile skill simulator`,
+  social: [
+    {
+      name: "github",
+      url: "https://github.com/brunoviveiros/rom-ss",
+    },
+    {
+      name: "twitter",
+      url: "https://twitter.com/brunoviveiros10",
+    },
+  ],
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,79 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const siteMetadata = require("./config/metadata")
+
 module.exports = {
-  /* Your site config here */
+  siteMetadata,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#7159c1`,
+        showSpinner: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [`400`, `500`, `700`],
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `ROM - Skill Simulator`,
+        short_name: `ROMSS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/assets/icon.png`, //512x512
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: process.env.NODE_ENV !== "production",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        ignore: [`**/styles.js`],
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `romData`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
 }
